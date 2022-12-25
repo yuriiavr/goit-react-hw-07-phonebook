ce7e8af359d30e5011554814ea5af0277e217353
yuriiavr <yurii.avramets@gmail.com>
2022-12-25 15:46:55 +0300

    321312

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-07-phonebook/">
         <App />
       </BrowserRouter>
     </Provider>
